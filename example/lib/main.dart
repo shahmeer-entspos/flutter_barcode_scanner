64137525f080ff199aab8523d41cbb07de100e09
ShahmeerAhmad <112636214+shahmeer-entspos@users.noreply.github.com>
2024-07-23 15:34:10 +0500

    changes

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:custom_flutter_barcode_scanner/custom_flutter_barcode_scanner.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 
 void main() => runApp(MyApp());
 
